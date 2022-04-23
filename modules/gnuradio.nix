7298701207d2d555e9486de61a1eade40ff3d080
revol-xut <revol-xut@protonmail.com>
2022-04-23 18:03:17 +0200

    updated flake with working gnuradio decoder

--- modules/gnuradio.nix
+++ modules/gnuradio.nix
@@ -2,23 +2,26 @@
   This file contains the configuration for the gnuradio sdr decoding pipeline
 */
 
-{ pkgs, config, lib }: {
+{ pkgs, config, lib, ... }: {
   systemd = {
     services."gnu-radio" = {
       enable = true;
       wantedBy = [ "multi-user.target" ];
+
       script = ''
         ${pkgs.gnuradio-decode}/bin/recv_and_demod.py
       '';
 
       serviceConfig = {
+        Forking = true;
         User = "gnuradio";
         Restart = "always";
       };
     };
   };
+
   users.users.gnu-radio = {
-    name = "gnu-radio";
+    name = "gnuradio";
     description = "gnu radio service user";
     isNormalUser = true;
   };
