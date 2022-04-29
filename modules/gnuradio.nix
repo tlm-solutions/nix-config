852d1ebd24a7040cb7d67540cf655d0ce7114adf
revol-xut <revol-xut@protonmail.com>
2022-04-29 23:31:21 +0200

    fixed url

--- modules/gnuradio.nix
+++ modules/gnuradio.nix
@@ -26,7 +26,7 @@ in {
         enable = true;
         wantedBy = [ "multi-user.target" ];
 
-        script = "exec ${pkgs.telegram-decoder}/bin/telegram-decode --config ${file} --server 10.13.37.1:8080 &";
+        script = "exec ${pkgs.telegram-decoder}/bin/telegram-decode --config ${file} --server http://10.13.37.1:8080 &";
 
         serviceConfig = {
           Type = "forking";
