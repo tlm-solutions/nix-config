286ff0813cd69a5f3b4af8b2d399fbd0286b03eb
Markus Schmidl <marenz2569@users.noreply.github.com>
2022-05-05 23:19:45 +0200

    Update partition.sh

--- partition.sh
+++ partition.sh
@@ -1,19 +1,17 @@
+parted /dev/mmcblk0 -- mklabel gpt
+parted /dev/mmcblk0 -- mkpart ESP fat32 1MiB 256MiB
+parted /dev/mmcblk0 -- set 1 esp on
+parted /dev/mmcblk0 -- mkpart primary 256MiB
 
-parted /dev/mmcblk -- mklabel gpt
-parted /dev/mmcblk -- mkpart ESP fat32 1MiB 512MiB
-parted /dev/mmcblk -- set 3 esp on
-parted /dev/mmcblk -- mkpart primary 512MiB.
-
-mkfs.fat -F 32 -n boot /dev/mmcblk1
-mkfs.ext4 -L root /dev/mmcblk2
+mkfs.fat -F 32 -n boot /dev/mmcblk0p1
+mkfs.ext4 -L nixos /dev/mmcblk0p2
 
 mount /dev/disk/by-label/root /mnt
 mkdir -p /mnt/boot
 mount /dev/disk/by-label/boot /mnt/boot
 
 cd /etc/nixos/
+rm /etc/nixos/*
 git init
 git remote add origin https://github.com/dump-dvb/nix-config.git
 git pull
-
-
