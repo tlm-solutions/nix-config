d168619e4fe08a1472c873afd8e9f85741180918
Grigory Shipunov <blame@oxapentane.com>
2022-05-27 23:49:08 +0200

    fix firewall

--- hosts/data-hoarder/configuration.nix
+++ hosts/data-hoarder/configuration.nix
@@ -44,7 +44,8 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 80 443 22 8080 51820 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 22 51820 ];
+  networking.firewall.trustedInterfaces = [ "wg-dvb" ];
   networking.firewall.allowedUDPPorts = [ 22 51820 ];
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
