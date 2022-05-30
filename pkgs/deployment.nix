88ef87505c86a7a715adf8a390a40a7f84d1d4b3
Markus Schmidl <marenz2569@users.noreply.github.com>
2022-05-30 20:50:18 +0200

    Refactor deployment (#8)
    * move deployment scripts
    
    * refactor deployment

--- pkgs/deployment.nix
+++ pkgs/deployment.nix
@@ -1,39 +1,36 @@
-{ self, pkgs, lib, boxes }:
+{ self, pkgs, systems }:
 let
-  # command which generates the update script for that specific machine
-  installScript = (target: (pkgs.writeScript "deploy" ''
-    #!${pkgs.runtimeShell}
-    ssh root@10.13.37.${toString (target + 100)} "ps cax | grep \"nixos-rebuild\" > /dev/null"
-    if [ $? -eq 0 ]
-    then
-        echo "Process is running."
-        exit
-    else
-        echo "Process is not running."
-        nix copy --to ssh://root@10.13.37.${toString (target + 100)} ${self}
-        ssh root@10.13.37.${toString (target + 100)} -- nixos-rebuild switch --flake ${self}#traffic-stop-box-${toString target}
-    fi
-  ''));
 
-  # concatanes commands together
-  deployBoxes = (systems: lib.strings.concatStringsSep " "
-    (builtins.map (system: "${(installScript system)}") systems));
+  installScript = (system:
+    let ip = "10.13.37.${toString (system.config.dvb-dump.systemNumber + 100)}";
+    in (pkgs.writeScriptBin "deploy" ''
+      #!${pkgs.runtimeShell}
+      ssh root@${ip} "ps cax | grep \"nixos-rebuild\" > /dev/null"
+      if [ $? -eq 0 ]
+      then
+          echo "\e[1;31m [!] nixos-rebuild is already running on ${ip}"
+          exit 1
+      else
+          nix copy --to ssh://root@${ip} ${self}
+          ssh root@${ip} -- nixos-rebuild switch --flake ${self} -L
+      fi
+    ''));
 
-  deployAllScript = (pkgs.writeScript "deploy-all" (
-    '' 
-              #!${pkgs.runtimeShell} -ex
-              ${pkgs.parallel}/bin/parallel --citation
-              ${pkgs.parallel}/bin/parallel -j10 ::: ${deployBoxes boxes} || echo "Some deployment failed"
-      ''
-  ));
+  installScripts = pkgs.lib.mapAttrs' (name: system:
+    pkgs.lib.attrsets.nameValuePair ("deploy-" + name) (installScript system))
+    systems;
 
-  individualScripts = lib.foldl (x: y: lib.mergeAttrs x y) { } (builtins.map (number: { "deploy-box-${toString number}" = (installScript number); }) boxes);
+  deployAllExecutablePathsConcatted =
+    pkgs.lib.strings.concatMapStringsSep " " (path: "${path}/bin/deploy")
+    (builtins.attrValues installScripts);
 
-in
-({
-  deploy-all = deployAllScript;
-}) #individualScripts
+  deployAllScript = (name:
+    pkgs.writeScriptBin name (''
+      #!${pkgs.runtimeShell} -ex
+      ${pkgs.parallel}/bin/parallel --will-cite -j10 ::: ${deployAllExecutablePathsConcatted} || echo "Some deployment failed"
+    ''));
 
-#in (individualScripts // {
-#  deploy-all = deployAllScript;
-#})
+in {
+  deploy-all = deployAllScript "deploy-all";
+  nuke-all = deployAllScript "nuke-all";
+} // installScripts
