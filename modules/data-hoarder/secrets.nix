9a3bf8a568bf638a4546218598f18fcdb2fb5285
revol-xut <revol-xut@protonmail.com>
2022-06-11 01:37:41 +0200

    group owned postgres password

--- modules/data-hoarder/secrets.nix
+++ modules/data-hoarder/secrets.nix
@@ -2,12 +2,20 @@
 {
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
+  users.groups = {
+    postgres-dvbdump = {
+      name = "postgres-dvbdump";
+      members = [ config.users.users.clicky-bunty-server.name config.users.users.data-accumulator.name ];
+    };
+  };
+
   sops.secrets = {
     wg-seckey = { };
     postgres_password_hash_salt = {
       owner = config.users.users.clicky-bunty-server.name;
     };
     postgres_password = {
+      group = config.users.groups.postgres-dvbdump.name;
       owner = config.users.users.clicky-bunty-server.name;
     };
   };
