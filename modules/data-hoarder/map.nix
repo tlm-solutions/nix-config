d2b6d19e7290e1295b7e9edce3327618829bbebd
revol-xut <revol-xut@protonmail.com>
2022-07-28 22:36:56 +0200

    add autoindex for testing

--- modules/data-hoarder/map.nix
+++ modules/data-hoarder/map.nix
@@ -10,6 +10,10 @@
         "map.${config.dump-dvb.domain}" = {
           forceSSL = true;
           enableACME = true;
+          extraConfig = ''
+            autoindex on;
+          '';
+
           locations = {
             "/" = {
               root = if (config.dump-dvb.domain == "dvb.solutions") then "${pkgs.windshield}/bin/" else "${pkgs.windshield-staging}/bin/";
