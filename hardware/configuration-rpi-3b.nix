7961371ded5591e565078fa1538b35b00c45059f
Grigory Shipunov <blame@oxapentane.com>
2022-08-24 14:45:40 +0200

    debugging

--- hardware/configuration-rpi-3b.nix
+++ hardware/configuration-rpi-3b.nix
@@ -1,28 +1,10 @@
 { lib, pkgs, ... }:
 {
   boot = {
-    loader = {
-      grub.enable = false;
-      raspberryPi.firmwareConfig = ''
-        hdmi_force_hotplug = 1
-        '';
-      # raspberryPi = {
-      #   enable = true;
-      #   version = 4;
-      # };
-    };
     kernelPackages = pkgs.linuxPackages_latest;
-    # No ZFS on latest kernel:
     supportedFilesystems = lib.mkForce [ "vfat" "ext4" ];
   };
 
-  # sdImage = {
-  #   compressImage = false;
-  #   imageBaseName = config.networking.hostName;
-  #   firmwareSize = 512;
-  # };
-
   networking.useDHCP = false;
   networking.interfaces.eth0.useDHCP = true;
-    # boot.loader.generic-extlinux-compatible.enable = lib.mkForce false;
 }
