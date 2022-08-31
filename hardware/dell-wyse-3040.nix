a0d09a3bfbb0a87ecd6c1c74f232bb4453390f3e
Grigory Shipunov <blame@oxapentane.com>
2022-08-31 13:44:14 +0200

    static network at location no. 4

--- hardware/dell-wyse-3040.nix
+++ hardware/dell-wyse-3040.nix
@@ -4,8 +4,25 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp1s0.useDHCP = true;
+
+  networking = if config.dump-dvb.systemNumber != 4 then {
+    interfaces.enp1s0.useDHCP = true;
+    useDHCP = false;
+  } else {
+    useDHCP = false;
+    defaultGateway = "141.30.30.129";
+    nameservers = [ "141.30.1.1" ];
+    interfaces.enp1s0 = {
+      useDHCP = false;
+      ipv4.addresses = [
+        {
+          address = "141.30.30.149";
+          prefixLength = 25;
+        }
+      ];
+    };
+  };
+
   boot.tmpOnTmpfsSize = "25%";
 
   boot.kernelModules = [ "kvm-intel" ];
