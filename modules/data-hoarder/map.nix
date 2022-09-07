beda761327de648bb7cef3e470adde1126658c9d
revol-xut <revol-xut@protonmail.com>
2022-09-07 22:51:18 +0200

    fixed endpoint in nginx

--- modules/data-hoarder/map.nix
+++ modules/data-hoarder/map.nix
@@ -16,11 +16,8 @@
               root = if (config.dump-dvb.domain == "dvb.solutions") then "${pkgs.windshield}/bin/" else "${pkgs.windshield-staging}/bin/";
               index = "index.html";
             };
-            "/stops/" = {
-              root = "${pkgs.stops}/stop/";
-            };
-            "/graph/" = {
-              root = "${pkgs.stops}/graph/";
+            "~ \.(json)" = {
+              root = "${pkgs.stops}/";
             };
           };
         };
