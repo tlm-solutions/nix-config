5e4fa14b1fc35545b5dffba217f29953f312a685
revol-xut <revol-xut@protonmail.com>
2022-09-07 22:13:52 +0200

    nginx endpoints

--- modules/data-hoarder/map.nix
+++ modules/data-hoarder/map.nix
@@ -16,11 +16,11 @@
               root = if (config.dump-dvb.domain == "dvb.solutions") then "${pkgs.windshield}/bin/" else "${pkgs.windshield-staging}/bin/";
               index = "index.html";
             };
-            "/stops.json" = {
-              root = "${pkgs.stops}/json";
+            "/stops/" = {
+              root = "${pkgs.stops}/json/";
             };
-            "/graph.json" = {
-              root = "${pkgs.stops}/json";
+            "/graph/" = {
+              root = "${pkgs.stops}/graph/";
             };
           };
         };
