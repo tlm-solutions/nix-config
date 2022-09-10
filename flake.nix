3098d70b1051a88e95bcb5b2b8b7911c9c5b27fc
Grigory Shipunov <blame@oxapentane.com>
2022-09-10 19:26:10 +0200

    traffic-stop-box-3: move network override to flake

--- flake.nix
+++ flake.nix
@@ -113,6 +113,23 @@
           extraModules = [
             ./hardware/dell-wyse-3040.nix
             dump-dvb.nixosModules.disk-module
+            {
+
+              networking = nixpkgs.lib.mkForce {
+                useDHCP = false;
+                defaultGateway = "141.30.30.129";
+                nameservers = [ "141.30.1.1" ];
+                interfaces.enp1s0 = {
+                  useDHCP = false;
+                  ipv4.addresses = [
+                    {
+                      address = "141.30.30.149";
+                      prefixLength = 25;
+                    }
+                  ];
+                };
+              };
+            }
           ];
         }
       ];
--- hardware/dell-wyse-3040.nix
+++ hardware/dell-wyse-3040.nix
@@ -5,22 +5,9 @@
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
 
-  networking = if config.dump-dvb.systemNumber != 4 then {
+  networking = {
     interfaces.enp1s0.useDHCP = true;
-    useDHCP = false;
-  } else {
-    useDHCP = false;
-    defaultGateway = "141.30.30.129";
-    nameservers = [ "141.30.1.1" ];
-    interfaces.enp1s0 = {
-      useDHCP = false;
-      ipv4.addresses = [
-        {
-          address = "141.30.30.149";
-          prefixLength = 25;
-        }
-      ];
-    };
+    useDHCP = true;
   };
 
   boot.tmpOnTmpfsSize = "25%";
