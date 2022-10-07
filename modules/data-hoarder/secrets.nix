42d8874f7e70af31950d7838b5ae4f9abf35f22f
revol-xut <revol-xut@protonmail.com>
2022-10-08 01:20:47 +0200

    adjusting permissions for postgres pw

--- modules/data-hoarder/secrets.nix
+++ modules/data-hoarder/secrets.nix
@@ -2,6 +2,7 @@
 let
   clicky-bunty-user = config.dump-dvb.clickyBuntyServer.user;
   data-accumulator-user = config.dump-dvb.dataAccumulator.user;
+  trekkie-user = config.dump-dvb.trekkie.user;
 in
 {
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
@@ -9,8 +10,10 @@ in
   users.groups = {
     postgres-dvbdump = {
       name = "postgres-dvbdump";
-      members = [ clicky-bunty-user data-accumulator-user "postgres" ];
+      members = [ clicky-bunty-user data-accumulator-user trekkie-user "postgres" ];
     };
+
+    #TODO: remove this the two databases got merged
     postgres-telegrams = {
       name = "postgres-telegrams";
       members = [ clicky-bunty-user data-accumulator-user "postgres" ];
