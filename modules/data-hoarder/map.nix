2b5d8d1b7d8e884b0933ef430bac82bb57857833
revol-xut <revol-xut@protonmail.com>
2022-11-18 21:49:29 +0100

    fixed map 404

--- modules/data-hoarder/map.nix
+++ modules/data-hoarder/map.nix
@@ -13,7 +13,7 @@
 
           locations = {
             "/" = {
-              root = if (config.deployment-dvb.domain == "dvb.solutions") then "${pkgs.windshield}/tarballs/" else "${pkgs.windshield-staging}/bin/";
+              root = if (config.deployment-dvb.domain == "dvb.solutions") then "${pkgs.windshield}/bin/" else "${pkgs.windshield-staging}/bin/";
               index = "index.html";
 
               tryFiles = "$uri /index.html =404";
