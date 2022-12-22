ea106010cdcc89738e13f958f07a41453253fdd0
revol-xut <revol-xut@protonmail.com>
2022-12-22 03:29:53 +0100

    adding dump-dvb base to watch-me-senpai

--- modules/watch-me-senpai/default.nix
+++ modules/watch-me-senpai/default.nix
@@ -1,6 +1,18 @@
 {pkgs, config, lib, ...}: {
   imports = [
-    #./wireguard_server.nix
-    #./secrets.nix
+    ../dump-dvb/base.nix
   ];
+
+  sops.defaultSopsFile = self + /secrets/watch-me-senpai/secrets.yaml;
+  deployment-dvb.net = {
+    wg = {
+      addr4 = "10.13.37.6";
+      prefix4 = 24;
+      privateKeyFile = config.sops.secrets.wg-seckey.path;
+      publicKey = "aNd+oXT3Im3cA0EqK+xL+MRjIx4l7qcXZk+Pe2vmRS8=";
+    };
+
+  };
+
+  deployment-dvb.domain = "dvb.solutions";
 }
