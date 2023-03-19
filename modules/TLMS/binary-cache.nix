84d53dd60be59065df565f91c173b6f21515a51b
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2023-03-19 21:22:51 +0100

    update cachix

--- modules/TLMS/binary-cache.nix
+++ modules/TLMS/binary-cache.nix
@@ -11,11 +11,11 @@
   config = lib.mkIf config.TLMS.useBinaryCache {
     nix.settings = {
       substituters = [
-        "https://dump-dvb.cachix.org"
+        "https://tlm-solutions.cachix.org"
         "https://hydra.hq.c3d2.de"
       ];
       trusted-public-keys = [
-        "dump-dvb.cachix.org-1:+Dq7gqpQG4YlLA2X3xJsG1v3BrlUGGpVtUKWk0dTyUU="
+        "tlm-solutions.cachix.org-1:J7qT6AvoNWPSj+59ed5bNESj35DLJNaROqga1EjVIoA="
         "nix-serve.hq.c3d2.de:KZRGGnwOYzys6pxgM8jlur36RmkJQ/y8y62e52fj1ps="
       ];
     };
