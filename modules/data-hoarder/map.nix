d04e8c10744595e04daa86b6f47f5686d2718231
Grigory Shipunov <blame@oxapentane.com>
2023-04-18 22:13:23 +0200

    redirect map to kindergarten

--- modules/data-hoarder/map.nix
+++ modules/data-hoarder/map.nix
@@ -8,61 +8,15 @@
           enableACME = true;
           forceSSL = true;
           extraConfig = ''
-            rewrite ^ https://map.${config.deployment-TLMS.domain}$request_uri permanent;
+            rewrite ^ https://kid.${config.deployment-TLMS.domain}/map/ permanent;
           '';
         };
         "map.${config.deployment-TLMS.domain}" = {
-          forceSSL = true;
           enableACME = true;
+          forceSSL = true;
           extraConfig = ''
-            autoindex on;
+            rewrite ^ https://kid.${config.deployment-TLMS.domain}/map/ permanent;
           '';
-
-          locations =
-            let
-              nginx_config = ''
-                # Permissions Policy - gps only
-                add_header Permissions-Policy "geolocation=()";
-
-                # Minimize information leaked to other domains
-                add_header 'Referrer-Policy' 'origin-when-cross-origin';
-
-                # Disable embedding as a frame
-                add_header X-Frame-Options DENY;
-
-                # Prevent injection of code in other mime types (XSS Attacks)
-                add_header X-Content-Type-Options nosniff;
-
-                # Enable XSS protection of the browser.
-                # May be unnecessary when CSP is configured properly (see above)
-                add_header X-XSS-Protection "1; mode=block";
-
-                # STS
-                add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
-                add_header "Access-Control-Allow-Origin" "*";
-              '';
-              json_dump = "/var/lib/json_dump/";
-            in
-            {
-              "/" = {
-                root = if (config.deployment-TLMS.domain == "tlm.solutions") then "${pkgs.windshield}/bin/" else "${pkgs.windshield-staging}/bin/";
-                index = "index.html";
-
-                tryFiles = "$uri /index.html =404";
-              };
-              "~ ^/stop/.*\.json$" = {
-                root = json_dump;
-                extraConfig = nginx_config;
-              };
-              "~ ^/graph/.*\.json$" = {
-                root = json_dump;
-                extraConfig = nginx_config;
-              };
-              "~ ^/region/.*\.json$" = {
-                root = json_dump;
-                extraConfig = nginx_config;
-              };
-            };
         };
       };
     };
