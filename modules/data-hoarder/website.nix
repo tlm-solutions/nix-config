b75e3a8e29ea8c9c77a9d3145a83bcc1d804c385
Grigory Shipunov <blame@oxapentane.com>
2023-04-18 20:57:32 +0200

    try redirect

--- modules/data-hoarder/website.nix
+++ modules/data-hoarder/website.nix
@@ -2,6 +2,17 @@
   services.nginx = {
     enable = true;
     virtualHosts = {
+      "~^(?<subdomain>\w+)\.dvb\.solutions$" = {
+        enableACME = true;
+        forceSSL = true;
+        locations = {
+          "/" = {
+            extraConfig = ''
+              rewrite ^ https://$subdomain.${config.deployemnt-TLMS.domain}$request_uri permanent;
+            '';
+          };
+        };
+      };
       "${config.deployment-TLMS.domain}" = {
         enableACME = true;
         forceSSL = true;
