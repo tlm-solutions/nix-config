310162c24fef2f8f944d8f856fc4bce74bda81aa
Grigory Shipunov <blame@oxapentane.com>
2023-05-04 17:00:51 +0200

    add node exporter to all deployment systems

--- modules/TLMS/default.nix
+++ modules/TLMS/default.nix
@@ -1,10 +1,12 @@
 {
-  imports = [
-    ./base.nix
-    ./binary-cache.nix
-    ./general-options.nix
-    ./json.nix
-    ./net.nix
-    ./wg.nix
-  ];
+  imports =
+    [
+      ./base.nix
+      ./binary-cache.nix
+      ./general-options.nix
+      ./json.nix
+      ./net.nix
+      ./wg.nix
+      ./node-exporter.nix
+    ];
 }
---
+++ modules/TLMS/node-exporter.nix
@@ -0,0 +1,14 @@
+{ config, ... }:
+{
+  # metrics exporter
+  services.prometheus.exporters = {
+    node = {
+      enable = true;
+      port = 8119;
+      listenAddress = config.deployment-TLMS.net.wg.addr4;
+      enabledCollectors = [
+        "systemd"
+      ];
+    };
+  };
+}
