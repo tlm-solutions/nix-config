7e25e245f5b32375ee4815226822d0e190bde64f
Grigory Shipunov <blame@oxapentane.com>
2023-05-19 19:08:52 +0200

    add borzoi

---
+++ hosts/tram-borzoi/default.nix
@@ -0,0 +1,100 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, self, ... }:
+let
+  mac_addr = "00:de:5b:f9:e2:fe";
+in
+{
+  imports = [
+    ./borzoi.nix
+    ./postgres.nix
+  ];
+  microvm = {
+    vcpu = 2;
+    mem = 1024 * 2;
+    hypervisor = "cloud-hypervisor";
+    socket = "${config.networking.hostName}.socket";
+
+    interfaces = [{
+      type = "tap";
+      id = "serv-dvb-borz";
+      mac = mac_addr;
+    }];
+
+    shares = [{
+      source = "/nix/store";
+      mountPoint = "/nix/.ro-store";
+      tag = "store";
+      proto = "virtiofs";
+      socket = "store.socket";
+    }
+      {
+        source = "/var/lib/microvms/tram-borzoi/etc";
+        mountPoint = "/etc";
+        tag = "etc";
+        proto = "virtiofs";
+        socket = "etc.socket";
+      }
+      {
+        source = "/var/lib/microvms/tram-borzoi/var";
+        mountPoint = "/var";
+        tag = "var";
+        proto = "virtiofs";
+        socket = "var.socket";
+      }];
+  };
+
+  networking.hostName = "tram-borzoi";
+
+  time.timeZone = "Europe/Berlin";
+
+  networking.useNetworkd = true;
+
+
+  sops.defaultSopsFile = ../../secrets/tram-borzoi/secrets.yaml;
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+
+  sops.secrets.wg-seckey = {
+    owner = config.users.users.systemd-network.name;
+  };
+  deployment-TLMS.net = {
+    iface.uplink = {
+      name = "ens3";
+      mac = mac_addr;
+      matchOn = "mac";
+      useDHCP = false;
+      addr4 = "172.20.73.38/25";
+      dns = [ "172.20.73.8" "9.9.9.9" ];
+      routes = [
+        {
+          routeConfig = {
+            Gateway = "172.20.73.1";
+            GatewayOnLink = true;
+            Destination = "0.0.0.0/0";
+          };
+        }
+      ];
+    };
+
+    wg = {
+      addr4 = "10.13.37.8";
+      prefix4 = 24;
+      privateKeyFile = config.sops.secrets.wg-seckey.path;
+      publicKey = "wCW+r5kAaIarvZUWf4KsJNetyHobP0nNy5QOhqmsCCs=";
+    };
+
+  };
+
+  deployment-TLMS.domain = "staging.tlm.solutions";
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
+
+}
