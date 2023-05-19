717f85e10ce443320350699647938813c1c9f92e
Grigory Shipunov <blame@oxapentane.com>
2023-05-19 18:15:28 +0200

    remove dead code

--- modules/watch-me-senpai/secrets.nix
+++
@@ -1,9 +0,0 @@
-{ config, ... }:
-{
-  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-  sops.secrets = {
-    wg-seckey = {
-      owner = config.users.users.systemd-network.name;
-    };
-  };
-}
