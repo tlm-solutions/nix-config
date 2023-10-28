724efac6becd566100408f0f828e99c0e81aa0a0
revol-xut <revol-xut@protonmail.com>
2023-10-28 17:57:37 +0200

    updating pubkey

--- hosts/traffic-stop-box/8/default.nix
+++ hosts/traffic-stop-box/8/default.nix
@@ -9,5 +9,5 @@
   users.users.root.openssh.authorizedKeys.keys = [
   ];
 
-  deployment-TLMS.net.wg.publicKey = "WPrf3g/dFFJWeTCY9EwtIrNe7tqhMm7qL1LLLEFUulM=";
+  deployment-TLMS.net.wg.publicKey = "dL9JGsBhaTOmXgGEH/N/GCHbQgVHEjBvIMaRtCsHBHw=";
 }
