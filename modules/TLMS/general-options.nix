ed97405de6751cef941fda2edad74670b4a9630f
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2023-11-25 23:10:37 +0100

    remove obsolete options

--- modules/TLMS/general-options.nix
+++ modules/TLMS/general-options.nix
@@ -1,12 +1,6 @@
 { lib, ... }:
 with lib; {
   options = {
-    deployment-TLMS.systemNumber = mkOption {
-      type = types.int;
-      default = 0;
-      description = "number of the system";
-    };
-
     deployment-TLMS.domain = mkOption {
       type = types.str;
       default = "tlm.solutions";
